0633a70de86b2ffd20aa0ddffb8f3eb61e73c232
ybtccc <ybtccc@163.com>
2018-11-25 22:54:46 +0800

    registry

---
+++ com.ybtccc.parent/com.ybtccc.registry/src/main/java/com/ybtccc/registry/RegistryApplication.java
@@ -0,0 +1,20 @@
+package com.ybtccc.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Created by ybtccc , 2018/11/25
+ * RegistryApplication
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
