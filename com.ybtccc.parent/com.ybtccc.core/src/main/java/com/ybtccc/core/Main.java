69ce566dbbe94d2f8fd13ac531b2b1ee37a54f59
mumu <ybtccc@163.com>
2018-12-04 20:44:57 +0800

    update

---
+++ com.ybtccc.parent/com.ybtccc.core/src/main/java/com/ybtccc/core/Main.java
@@ -0,0 +1,11 @@
+package com.ybtccc.core;
+
+/**
+ * Main Created by ybtccc. 2018/12/4
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ com.ybtccc.parent/com.ybtccc.registry/src/main/java/com/ybtccc/registry/util/Hello.java
@@ -0,0 +1,14 @@
+package com.ybtccc.registry.util;
+
+import java.util.Calendar;
+
+/**
+ * Hello Created by ybtccc. 2018/12/3
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        System.out.println(hour);
+    }
+}
