e3c2d5ec2c9ee1b2e746f652d0f4f38daf312f33
mumu <ybtccc@163.com>
2018-12-05 15:48:15 +0800

    update

--- com.ybtccc.parent/com.ybtccc.service/src/main/java/com/ybtccc/service/service/EchoServiceImpl.java
+++ com.ybtccc.parent/com.ybtccc.service/src/main/java/com/ybtccc/service/service/EchoServiceImpl.java
@@ -3,10 +3,12 @@ package com.ybtccc.service.service;
 import com.ybtccc.api.service.EchoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * EchoServiceImpl Created by ybtccc. 2018/12/5
  */
+@RestController
 public class EchoServiceImpl implements EchoService {
 
     private static Logger logger = LoggerFactory.getLogger(EchoService.class);
