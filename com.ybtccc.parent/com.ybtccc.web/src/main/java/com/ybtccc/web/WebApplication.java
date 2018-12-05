9a57eeaa9a20dabb8682c4f180fc74937e7095a7
mumu <ybtccc@163.com>
2018-12-05 09:51:21 +0800

    update

--- com.ybtccc.parent/com.ybtccc.web/src/main/java/com/ybtccc/web/WebApplication.java
+++ com.ybtccc.parent/com.ybtccc.web/src/main/java/com/ybtccc/web/WebApplication.java
@@ -1,7 +1,9 @@
 package com.ybtccc.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,6 +21,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        new SpringApplicationBuilder(WebApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
