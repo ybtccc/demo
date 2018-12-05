ea97dc98b64142b9628a03d916fd43ef98e3c468
mumu <ybtccc@163.com>
2018-12-05 18:56:31 +0800

    update ,网关配置

--- com.ybtccc.parent/com.ybtccc.gateway/src/main/java/com/ybtccc/gateway/GatewayApplication.java
+++ com.ybtccc.parent/com.ybtccc.gateway/src/main/java/com/ybtccc/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.ybtccc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * GatewayApplication Created by ybtccc. 2018/12/5
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
